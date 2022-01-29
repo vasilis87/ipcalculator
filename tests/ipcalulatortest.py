9a849a1b791728968833d230cb3ad9283f94ac8a
vasilis87 <vaskapart@gmail.com>
2022-01-29 16:03:33 +0400

    test

--- tests/ipcalulatortest.py
+++ tests/ipcalulatortest.py
@@ -1,5 +1,6 @@
 from unittest import TestCase,main
-from ipcalc import ipcalculator
+
+
 class Ipcalculator_test(TestCase):
     def test_24(self):
         self.assertEqual(ipcalculator('192.168.1.5/24'),('192.168.1.0','255.255.255.0','0.0.0.255','192.168.1.255','192.168.1.1','192.168.1.254'))
